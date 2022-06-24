b4a25f4b47a476f87f9c0b80fe2587c0866d8f43
Vinicius-Santos26 <vinioliv.santos@outlook.com>
2022-06-24 17:12:37 -0300

    Fim aula 4

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o274z907i001xmgekp7azx/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
